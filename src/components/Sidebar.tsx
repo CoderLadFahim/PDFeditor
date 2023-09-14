b15c53a7d710fcb3c688e0810f393dba625e8c04
fahimalemroz <fahimalemroz@gmail.com>
2023-09-14 14:13:04 +0600

    designed the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,17 @@
+import { Edit, Upload } from "react-feather"
+
 function Sidebar() {
 	return (
-		<nav className="app-sidebar bg-sky-900 text-white">
+		<nav className="app-sidebar bg-gray-200 text-white">
 			<ul>
-				<li className="bg-sky-800 py-24">Upload</li>
-				<li className="bg-sky-800">Compose</li>
+				<li className="active-nav-link nav-link">
+				    <Upload />
+				    <p className="nav-link-text">Upload</p>
+				</li>
+                <li className= "nav-link">
+				    <Edit />
+				    <p className="nav-link-text">Compose</p>
+				</li>
 			</ul>
 		</nav>
 	)

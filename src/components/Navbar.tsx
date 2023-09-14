31d155339ef853e11a906e9623eeb3371a9ce43a
fahimalemroz <fahimalemroz@gmail.com>
2023-09-14 13:23:14 +0600

    Created a very basic layout

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+        <nav className="app-navbar bg-gray-200 w-100">
+            <div className="logo-wrapper bg-gray-300 grid place-items-center">Sigma</div>
+        </nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,12 @@
+function Sidebar() {
+	return (
+		<nav className="app-sidebar bg-sky-900 text-white">
+			<ul>
+				<li className="bg-sky-800 py-24">Upload</li>
+				<li className="bg-sky-800">Compose</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Sidebar

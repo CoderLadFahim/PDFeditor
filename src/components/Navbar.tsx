d8a5c1df88c83ef6ec2b5e9cac80f472bba3cb0a
fahimalemroz <fahimalemroz@gmail.com>
2023-09-16 21:54:59 +0600

    Added a logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,8 +6,8 @@ function Navbar() {
 
 	return (
 		<nav className={`app-navbar transition bg-gray-200 w-100 z-10 ${state.previewMode ? 'hidden' : '' }`}>
-			<div className="logo-wrapper bg-gray-400 grid text-white place-items-center text-2xl">
-				LOGO
+			<div className="logo-wrapper bg-gray-50 grid text-white place-items-center text-2xl">
+				<img className='w-52'  src="https://jouleslabs.com/wp-content/uploads/2021/07/cropped-JoulesLab-Logo-without-tagline.png" alt="applicaion logo" />
 			</div>
 		</nav>
 	)

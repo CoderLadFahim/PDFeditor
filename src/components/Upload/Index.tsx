718a36c6e2ace12276253aef167d247f462a5c03
fahimalemroz <fahimalemroz@gmail.com>
2023-09-16 15:51:31 +0600

    displaying the locallyStoredFiles

--- src/components/Upload/Index.tsx
+++ src/components/Upload/Index.tsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react'
 import 'react-dropzone-uploader/dist/styles.css'
 import Dropzone from 'react-dropzone-uploader'
+import { X } from 'react-feather'
 
 function Upload() {
 	const [fileStatuses, setFileStatuses] = useState<File[]>([])
@@ -48,7 +49,7 @@ function Upload() {
 
 	useEffect(() => {
 	    const filesLocallyStored: any = localStorage.getItem('fileData')
-	    setLocallyStoredFiles(() => filesLocallyStored)
+	    setLocallyStoredFiles(() => JSON.parse(filesLocallyStored))
 	}, [])
 
 	return (
@@ -70,6 +71,27 @@ function Upload() {
 					onChangeStatus={handleChangeStatus}
 					accept="application/pdf"
 				/>
+			</div>
+
+            <div className="mt-16">
+				<ul className="flex flex-wrap justify-between gap-y-7">
+					{locallyStoredFiles?.map((file: any, i: number) => (
+						<li className="rounded-md relative shadow p-2 w-[17rem] bg-white overflow-hidden group">
+							<span className="absolute top-0 bottom-0 left-0 w-[3rem] bg-sky-400 text-white grid place-items-center">
+								getFileType(file.path)
+							</span>
+							<span className="file-name  ml-[3rem]">
+								truncateString(file.path, 20)
+							</span>
+
+							<span className="absolute top-0 bottom-0 right-0 hidden w-[2rem] text-red-400 group-hover:grid place-items-center">
+								<button className="cursor-pointer">
+								    <X />
+								</button>
+							</span>
+						</li>
+					))}
+				</ul>
 			</div>
 		</div>
 	)

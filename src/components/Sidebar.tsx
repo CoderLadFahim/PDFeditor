f0c7e0bd38f878e533552f78c47d175582ced560
fahimalemroz <fahimalemroz@gmail.com>
2023-09-16 17:23:49 +0600

    Fixed the image not being deleted problem

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,13 +11,13 @@ function Sidebar() {
 	return (
 		<nav className={`app-sidebar bg-gray-900 z-10 text-white ${state.previewMode ? 'hidden' : '' }`}>
 			<ul>
-				<li className={`${location.pathname === '/' ? 'active-nav-link' : ''} nav-link`}>
-				    <Upload />
-				    <p className="nav-link-text"><Link to="/">Upload</Link></p>
-				</li>
                 <li className={`${location.pathname === '/compose' ? 'active-nav-link' : ''} nav-link`}>
 				    <Edit />
 				    <p className="nav-link-text"><Link to="/compose">Compose</Link></p>
+				</li>
+				<li className={`${location.pathname === '/' ? 'active-nav-link' : ''} nav-link`}>
+				    <Upload />
+				    <p className="nav-link-text"><Link to="/">Upload</Link></p>
 				</li>
 			</ul>
 		</nav>

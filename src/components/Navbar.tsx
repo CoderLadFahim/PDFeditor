c31c8b9a7f4089d452146279d5bd8393333c1f29
CoderLadFahim <fahimalemroz@gmail.com>
2024-04-08 15:02:37 +0600

    Updated the 2nd release

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,19 @@
 import { useContext } from 'react'
 import { CanvasContext } from '../contexts/CanvasContext'
-import { Settings } from 'react-feather';
+import { Settings, Info } from 'react-feather';
 
 function Navbar() {
-	const {state} = useContext(CanvasContext)
-	const activeDocument = state.documents.find(documentObj => documentObj.documentId === state.activeDocumentId);
+    const { state } = useContext(CanvasContext)
+    const activeDocument = state.documents.find(documentObj => documentObj.documentId === state.activeDocumentId);
 
-	return (
-		<nav className={`app-navbar shadow transition bg-gray-200 w-100 z-10 ${activeDocument?.previewMode ? 'hidden' : '' }`}>
-			<div className="logo-wrapper bg-gray-50 flex justify-center items-center text-2xl space-x-2">
-				<Settings className='text-green-500' />
-				<p className='font-bold text-gray-600 -skew-x-12'>PDF Mechanic</p>
-			</div>
-		</nav>
-	)
+    return (
+        <nav className={`app-navbar shadow transition bg-gray-200 w-100 z-10 flex justify-between items-center ${activeDocument?.previewMode ? 'hidden' : ''}`}>
+            <div className="logo-wrapper bg-gray-50 flex justify-center items-center text-2xl space-x-2">
+                <Settings className='text-green-500' />
+                <p className='font-bold text-gray-600 -skew-x-12'>PDF Mechanic</p>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
